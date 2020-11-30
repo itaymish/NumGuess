213a59557552d6ad4f06346068d7db1aee4da6b1
itaymish <62606669+itaymish@users.noreply.github.com>
2020-11-30 22:24:17 +0200

    Update NumGuess.py

--- NumGuess.py
+++ NumGuess.py
@@ -14,24 +14,27 @@ def main():
 
 	tries = 0
 
-	hlp = Label(root, text="Hi there!\nI picked a number between 1 to 100.\nEnter your guess:")
+	hlp = Label(root, text="I picked a whole number between 1 to 100.\nGuess what is it:")
 	hlp.pack()
 
 	eyg = Entry(root)
 	eyg.pack()
 
+	eyg.insert(0, ran)
 	def chk(guess):
 		global ran
 		global tries
 		global root
 
-		if int(guess) > int(ran):
+		if float(guess) > 100 or float(guess) < 1:
+			messagebox.showinfo("NumGuess", str(guess) + ", Your guess, is not between 1 to 100.\nPlease try another number.")
+		elif float(guess) > float(ran):
 			tries += 1
 			messagebox.showinfo("NumGuess", "Your guess is too high.\nPlease, try again.")
-		elif int(guess) < int(ran):
+		elif float(guess) < float(ran):
 			tries += 1
 			messagebox.showinfo("NumGuess", "Your guess is too low.\nPlease, try again.")
-		elif int(guess) == int(ran):
+		elif float(guess) == float(ran):
 			tries += 1
 			messagebox.showinfo("NumGuess", "Your guess is correct!.\nNumber or tries: " + str(tries) + ".")
 			pa = messagebox.askyesno("NumGuess", "Do you wanna play again?\nPress 'Yes or 'No'.")
@@ -40,13 +43,22 @@ def main():
 				hlp.pack_forget()
 				eyg.pack_forget()
 				snd.pack_forget()
+				clr.pack_forget()
 
 				main()
 			elif pa == 0:
 				root.destroy()
+		else:
+			messagebox.showinfo("NumGuess", str(guess) + ", Your guess, is unknown number.\nPlease try another number.")
+
+	def dlt():
+		eyg.delete(0, END)
 
 	snd = Button(root, text="Guess", command=lambda: chk(eyg.get()))
 	snd.pack()
+
+	clr = Button(root, text="Clear", command=dlt)
+	clr.pack()
 
 	root.mainloop()
 
